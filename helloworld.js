d1607df2fcf88558bed3ea98b8ae6ba98e3bcb25
geekysilento <adityakarna.ak@gmail.com>
2024-02-27 22:45:13 +0545

    initialized repo with broken code

---
+++ helloworld.js
@@ -0,0 +1,10 @@
+let messge = "Hello, world!";
+
+console.loog(messge);
+
+function add(numberr1, number2) {
+    return number1 + number2;
+}
+
+let result = add(3, 5);
+console.log("The result is: " + reslut);
