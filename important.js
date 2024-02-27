f9ccb11ad6cfc2102d6afd542ffdb595e9329b45
geekysilento <adityakarna.ak@gmail.com>
2024-02-27 22:51:26 +0545

    more broken code

---
+++ important.js
@@ -0,0 +1,43 @@
+
+
+
+let messge = "Hello, world!";
+
+
+consol.log(messge);
+
+
+fucntion add(numberr1, number2) {
+    return numer1 + numbre2;
+}
+
+
+let resutl = add(3, 5);
+cosole.log("The result is: " + resutl);
+
+
+let nmber = 10;
+
+
+let sum = nmber + 5;
+
+
+conole.log("The sum is: " + sum);
+
+
+let arry = [1, 2, 3, 4, 5];
+
+
+let elment = arry[10];
+
+
+conosle.log("The element is: " + elment);
+
+
+let objct = { name: "John", age: 30, };
+
+
+let ag = objct.agg;
+
+
+consoe.log("The age is: " + ag);
